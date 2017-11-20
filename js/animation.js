11ca6a66edd6a42134159acfb9da5b434bb7ace0
ledgil <2559860328@qq.com>
2017-11-20 17:37:49 +0800

    Site updated: 2017-11-20 17:37:48

--- js/animation.js
+++
@@ -1,9 +0,0 @@
-//animation
-$(function(){
-   $('.animation-item').hover(
-   	function(){
-    $(this).removeClass('itemRIn').addClass('itemLIn');   
-   },function(){
-   	$(this).removeClass('itemLIn').addClass('itemRIn');   
-   });  
-});
